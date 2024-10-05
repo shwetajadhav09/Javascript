27a2ae32856d0bd672e9b91d5816089480128365
Shweta Jadhav <117379986+shwetajadhav09@users.noreply.github.com>
2024-10-05 11:06:58 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,33 @@
+let score = null
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let value = undefined
+
+console.log(typeof value);
+
+let valueeInNumber = Number(value)
+console.log(typeof valueeInNumber);
+console.log(valueeInNumber)
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1
+//false => 0
+
+
+//to convert boolean:
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 =>true
+//0 =>false
+// "" => false
+// "shweta" => true
