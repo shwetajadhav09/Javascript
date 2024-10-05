8127cafd6676d633992520dfcc4da74fa6681057
Shweta Jadhav <117379986+shwetajadhav09@users.noreply.github.com>
2024-10-05 10:22:38 +0000

    added variables.js

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountID = 12345
+let accountEmail = "scj@gmail.com"
+var accountPassword = "scj"
+accountCity = "Pune"
+
+// accountID = 2 --> not allowed for const
+
+accountEmail = "sc@sc.com"
+accountPassword = "123"
+accountCity = "Bengaluru"
+
+console.table([accountID, accountEmail, accountPassword, accountCity]);
