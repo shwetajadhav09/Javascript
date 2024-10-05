e4051a689169898675044a63c864a50fabe66b0a
Shweta Jadhav <117379986+shwetajadhav09@users.noreply.github.com>
2024-10-05 10:41:16 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat the JS code as a newer version
+
+let name = "Shweta"
+let age = 20
+let isLoggedIn = true
+
+/*
+number -> 2 to the power 53
+bigint
+string -> ""
+boolean -> true/false
+null
+symbol -> unique
+
+*/
+
+console.log(typeof age)
+console.log(typeof undefined);
+console.log(typeof null)
