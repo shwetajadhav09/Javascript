ccecd80ffb8d0b94d37da45ec92a5fed2f26b465
Shweta Jadhav <117379986+shwetajadhav09@users.noreply.github.com>
2024-10-10 11:23:45 +0000

    made changes to datatypes Summary

--- 01_basics/datatypesSummary.js
+++ 01_basics/datatypesSummary.js
@@ -12,3 +12,12 @@ array, objects, functions
 javascript is a dynamically typed language which means datatypes of variables are determined by the value they hold at runtime
 and can change throughout the program as we assign different values to them
 */
+
+
+// ************************************************************************
+/*
+Stack->primitive, 
+heap -> non-primitive
+
+*/
+
