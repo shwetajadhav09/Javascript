5a3c637f2487ae57390e30263b710fbec1351ece
Shweta Jadhav <117379986+shwetajadhav09@users.noreply.github.com>
2024-10-18 17:35:44 +0000

    added math.js

---
+++ 01_basics/math.js
@@ -0,0 +1,30 @@
+const score = 400
+
+const balance = new Number(40);
+
+console.log(balance);
+
+console.log(balance.toString());
+
+console.log(balance.toFixed(3));
+
+const hundreds = 100000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++Math+++++++++++
+console.log(Math.abs(-1));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
+
