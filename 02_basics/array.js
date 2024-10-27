036fa810fc77a29218e4e3464530658d7fc9dc85
Shweta Jadhav <117379986+shwetajadhav09@users.noreply.github.com>
2024-10-27 15:11:38 +0000

    added a new folder

---
+++ 02_basics/array.js
@@ -0,0 +1,20 @@
+
+const myarr = [0,1,2,3,4,5]
+
+const arr = new Array(1,2,3,4)
+
+//array methods
+
+myarr.push(6);
+
+myarr.push(7);
+myarr.pop();
+console.log(myarr);
+
+myarr.unshift(4);
+
+const newarr = myarr.join(); //changes the typeof
+console.log(newarr)
+
+
+//slice, splice
