0b89893b12b1b43e85943ebe53aac57e01a7f341
Shweta Jadhav <117379986+shwetajadhav09@users.noreply.github.com>
2024-11-14 17:46:25 +0000

    made changes to 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -26,3 +26,32 @@ JSuser.greeting = function () {
 
 JSuser.greeting();
 JSuser.greetingTwo();
+
+
+//singleton
+
+const user = {};
+
+user.id = 123;
+user.name = "Shweta"
+user.isLoggedIn = false;
+
+const regularUser = {
+    email: "s@gmail.com",
+    name: {
+        userfullname: {
+                firstname: "Shweta",
+                lastname: "Jadhav"
+        }
+    }
+}
+
+const obj1 = {"a": 1, "b": 2};
+const obj2 = {"c": 1, "d": 2};
+
+const obj3 = Object.assign({}, obj1, obj2)
+console.log(obj3);
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(user.hasOwnProperty('isLoggedIn'));
